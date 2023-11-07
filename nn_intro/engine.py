0df1729a7da525678e5fa3da328b6ac01fd2f1cb
Viktor Brada <Joelino@users.noreply.github.com>
2023-11-07 17:08:58 +0000

    Add a support for addition and multiplication # TOPIC: overloading arithmetic operations

--- nn_intro/engine.py
+++ nn_intro/engine.py
@@ -6,3 +6,15 @@ class Value:
 
     def __repr__(self):
         return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        # other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data)
+
+        return out
+
+    def __mul__(self, other):
+        # other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data)
+
+        return out
