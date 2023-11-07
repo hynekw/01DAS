0dd2711d3ae5034b73779c0d955228e0ee7605a2
Viktor Brada <Joelino@users.noreply.github.com>
2023-11-07 17:08:06 +0000

    Create a class # TOPIC: classes, instances, variables # TOPIC: dundr methods, init, repr

---
+++ nn_intro/engine.py
@@ -0,0 +1,8 @@
+class Value:
+    """ stores a single scalar value and its gradient """
+
+    def __init__(self, data):
+        self.data
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
