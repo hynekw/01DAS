e3273b2a75427f6142984e65ece50edf2c701ec5
Viktor Brada <Joelino@users.noreply.github.com>
2023-11-07 17:10:14 +0000

    Add children and operations params

--- nn_intro/engine.py
+++ nn_intro/engine.py
@@ -1,21 +1,23 @@
 class Value:
     """ stores a single scalar value and its gradient """
 
-    def __init__(self, data):
-        self.data
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self, other), '+')
 
         return out
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self, other), '*')
 
         return out
 
