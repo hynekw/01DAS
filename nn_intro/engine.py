7275d3909ef87bc6a0125bf81af8df036f4c573a
Viktor Brada <Joelino@users.noreply.github.com>
2023-11-07 17:09:35 +0000

    Implement support for subtraction using addition

--- nn_intro/engine.py
+++ nn_intro/engine.py
@@ -8,13 +8,19 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        # other = other if isinstance(other, Value) else Value(other)
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data)
 
         return out
 
     def __mul__(self, other):
-        # other = other if isinstance(other, Value) else Value(other)
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data)
 
         return out
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
