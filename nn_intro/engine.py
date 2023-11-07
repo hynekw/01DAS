d8d4517962f90ff1430d7e7dafe2d7c551102aaf
Viktor Brada <Joelino@users.noreply.github.com>
2023-11-07 17:12:41 +0000

    Add a gradient calculation

--- nn_intro/engine.py
+++ nn_intro/engine.py
@@ -3,6 +3,9 @@ class Value:
 
     def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self.grad = 0
+        # internal variables used for autograd graph construction
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
@@ -13,11 +16,21 @@ class Value:
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+
         return out
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
 
         return out
 
